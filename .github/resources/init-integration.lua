2b098657c97db7a8b371f61b2f7b944c3d991c67
Benjamin Lakis <benlakis@student.purdueglobal.edu>
2025-03-11 08:41:58 -0500

    Update init-integration.lua

--- .github/resources/init-integration.lua
+++ .github/resources/init-integration.lua
@@ -5,21 +5,21 @@ vim.g.rocks_nvim = {
 }
 
 local luarocks_path = {
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.1", "?.lua"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.4", "?.lua"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "share", "lua", "5.4", "?", "init.lua"),
 }
 package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
 local luarocks_cpath = {
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.dylib"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.dylib"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.1", "?.dll"),
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.1", "?.dll"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.4", "?.so"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.4", "?.so"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.4", "?.dylib"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.4", "?.dylib"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "lua", "5.4", "?.dll"),
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib64", "lua", "5.4", "?.dll"),
 }
 package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
 vim.opt.runtimepath:append(
-    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*")
+    vim.fs.joinpath(vim.g.rocks_nvim.rocks_path, "lib", "luarocks", "rocks-5.4", "rocks.nvim", "*")
 )
